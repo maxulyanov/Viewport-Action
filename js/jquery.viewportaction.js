d788a500f082b2f8c3024154f444f94e0d931d2c
unknown <web.ulyanov@gmail.com>
2014-10-29 21:48:00 +0400

    Test Version 0.1
    1

---
+++ js/jquery.viewportaction.js
@@ -0,0 +1,65 @@
+/*
+ 
+*/
+
+(function($){
+    $.fn.viewportaction = function(options) {
+
+        // Опции по умолчанию
+        var options = $.extend({}, {
+            effect: 'visible',
+            offsetTop: 100,
+            repeat: true,
+        }, options);
+
+        // Запомним this и высоту страницу
+        var elem = this,
+            windowHeight =  $(window).height();
+
+        this.unitElem = function() {
+      
+  			// Значения отступов
+  			var scrollTop = $('body').scrollTop(),
+				scrollBottom = (scrollTop + windowHeight);
+			
+            
+            $(elem).each(function() {
+                var obj = $(this),
+                    objOpt = {};
+                $.extend(objOpt, options);
+
+                // Если класс уже существует -  выход
+                if($(obj).hasClass(objOpt.effect) && !objOpt.repeat){
+                    return;
+                }
+
+                // Позиция элемента + смещение
+                var elemTop =  Math.round($(obj).offset().top) + objOpt.offsetTop,
+                    elemBottom = elemTop + ($(obj).height());
+
+                // Применить эффект
+                if((elemTop < scrollBottom) && (elemBottom > scrollTop)){
+                    $(obj).addClass(objOpt.effect);
+                    
+                }
+                
+                // Удалить эффект, если параметр repeat == true 
+                else if($(obj).hasClass(objOpt.effect) && (objOpt.repeat)){
+                    $(obj).removeClass(objOpt.effect);
+                }
+            });
+        
+        };
+
+        // Запуск метода unitElem при загрузке и скролле страницы
+        $(window).on('load scroll touchmove', this.unitElem);
+        
+        this.unitElem();
+        
+        return this;
+    };
+
+})(jQuery);
+
+
+
